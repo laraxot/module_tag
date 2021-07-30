373afdcc19287c00630c86a168aae3faee8fc221
leonardo <marchesini.leonardo2002@gmail.com>
2021-07-30 15:57:01 +0200

    Aggiunti campi corretti su Tag per modulo Shop

--- Models/Tag.php
+++ Models/Tag.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Modules\Tag\Models;
 
 class Tag extends BaseModelLang {
-    protected $fillable = ['id', 'tag_type', 'tag_cat_id', 'old_id', 'pos'];
+    protected $fillable = ['id', 'parent_id','tag_type', 'tag_cat_id', 'old_id', 'pos'];
     //protected $guard = ['id'];
 
     public function treeLabel(): string {
@@ -15,4 +15,9 @@ class Tag extends BaseModelLang {
     public function tagCat() {
         return $this->belongsTo(TagCat::class);
     }
+
+    public function parent(){
+        return $this->hasOne(Tag::class, 'parent_id','id');
+    }
+
 }
