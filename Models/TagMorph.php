85e9849e91e682c92ab770d1bf2e97c4b5210140
marco76tv <marco.sottana@gmail.com>
2021-08-23 18:01:49 +0200

    .

--- Models/TagMorph.php
+++ Models/TagMorph.php
@@ -14,5 +14,5 @@ class TagMorph extends BaseMorphPivot {
      *
      * @var array
      */
-    protected $fillable = [];
+    protected $fillable = ['id', 'tag_id', 'post_type', 'post_id', 'created_at', 'updated_at', 'created_by', 'updated_by', 'auth_user_id', 'note'];
 }
