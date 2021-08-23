2be73a16a2d7cd9847b43571c39be5e47afbefff
Malebestia <malebestia@yahoo.it>
2021-08-23 20:48:38 +0200

    .

--- Models/Tag.php
+++ Models/Tag.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Tag\Models;
 
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
 class Tag extends BaseModelLang {
     protected $fillable = ['id', 'parent_id','tag_type', 'tag_cat_id', 'old_id', 'pos'];
     //protected $guard = ['id'];
@@ -15,25 +12,12 @@ class Tag extends BaseModelLang {
         return (string) optional($this->post)->title;
     }
 
-    public function tagCat():BelongsTo{
+    public function tagCat() {
         return $this->belongsTo(TagCat::class);
     }
 
-    public function parent():HasOne{
+    public function parent(){
         return $this->hasOne(Tag::class, 'parent_id','id');
-    }
-
-    public function products() {
-        $pivot = app(TagMorph::class);
-        $pivot_fields = $pivot->getFillable();
-        $pivot_table = $pivot->getTable();
-
-        return $this->morphedByMany(\Modules\Shop\Models\Product::class, 'post', $pivot_table)
-            ->using($pivot)
-            ->withPivot($pivot_fields)
-            ->withTimestamps()
-            ->with(['post']) //Eager;
-            ;
     }
 
 }
