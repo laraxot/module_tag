78468ad3a370d8764e64aec97323a57b282535a7
marco76tv <marco.sottana@gmail.com>
2021-08-23 12:37:40 +0200

    .

--- Models/Panels/TagPanel.php
+++ Models/Panels/TagPanel.php
@@ -65,8 +65,8 @@ class TagPanel extends XotBasePanel {
     /**
      * index navigation.
      */
-    public function indexNav():?array {
-        return [];
+    public function indexNav(): ?\Illuminate\Contracts\Support\Renderable {
+        return null;
     }
 
     /**
@@ -109,7 +109,7 @@ class TagPanel extends XotBasePanel {
                 'comment' => null,
                 'col_bs_size' => 5,
             ],
-             
+
             (object) [
                 'type' => 'String',
                 'name' => 'post.title',
@@ -131,7 +131,6 @@ class TagPanel extends XotBasePanel {
                 'comment' => null,
                 'col_bs_size' => 2,
             ],
-
         ];
     }
 
