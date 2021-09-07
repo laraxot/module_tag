202d4366d8016c56baea6e69ee35e2940cbe30dd
Malebestia <malebestia@yahoo.it>
2021-09-07 19:21:06 +0200

    .

--- Models/TagMorph.php
+++ Models/TagMorph.php
@@ -6,12 +6,6 @@ namespace Modules\Tag\Models;
 
 class TagMorph extends BaseMorphPivot {
     /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-
-    /**
      * Attributes that should be mass-assignable.
      *
      * @var array
