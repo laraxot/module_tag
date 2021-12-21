5dab3b15633ba7ebb9fe01908ffae99768349757
marco76tv <marco.sottana@gmail.com>
2021-12-21 17:33:26 +0100

    .

--- Models/Tag.php
+++ Models/Tag.php
@@ -8,38 +8,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Modules\Blog\Models\Article;
+use Modules\Blog\Models\Traits\HasSlug;
 use Modules\Tenant\Services\TenantService;
 
 /**
- * Modules\Tag\Models\Tag.
- *
- * @property string|null                                                          $guid
- * @property string|null                                                          $image_src
- * @property string|null                                                          $lang
- * @property string|null                                                          $post_type
- * @property string|null                                                          $subtitle
- * @property string|null                                                          $title
- * @property string|null                                                          $txt
- * @property string|null                                                          $user_handle
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
- * @property int|null                                                             $images_count
- * @property Tag|null                                                             $parent
- * @property \Modules\Blog\Models\Post|null                                       $post
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Blog\Models\Post[] $posts
- * @property int|null                                                             $posts_count
- * @property mixed                                                                $url
- * @property \Modules\Tag\Models\TagCat                                           $tagCat
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- * @mixin \Eloquent
+ * Undocumented class.
  */
-
- 
 class Tag extends BaseModelLang {
+    use HasSlug;
+
     protected $fillable = ['id', 'parent_id', 'tag_type', 'tag_cat_id', 'old_id', 'pos'];
 
     //protected $guard = ['id'];
