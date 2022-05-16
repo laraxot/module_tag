883ae2ecb21ec8bca5d527fff1610c707622b8b3
Malebestia <malebestia@yahoo.it>
2022-05-16 22:37:34 +0200

    .

--- Models/Tag.php
+++ Models/Tag.php
@@ -10,4 +10,5 @@ namespace Modules\Tag\Models;
 use Spatie\Tags\Tag as BaseTag;
 
 class Tag extends BaseTag {
+    protected $connection = 'tag'; // this will use the specified database connection
 }
