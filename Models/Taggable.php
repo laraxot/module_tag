70a61f5f8ac5af8913f1ea599352e67fce74b349
marco76tv <marco.sottana@gmail.com>
2022-06-07 13:44:48 +0200

    up

--- Models/Taggable.php
+++ Models/Taggable.php
@@ -7,9 +7,15 @@ namespace Modules\Tag\Models;
 /**
  * Modules\Tag\Models\Taggable
  *
+ * @property int $tag_id
+ * @property string $taggable_type
+ * @property int $taggable_id
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Taggable whereTagId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Taggable whereTaggableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Taggable whereTaggableType($value)
  * @mixin \Eloquent
  */
 class Taggable extends BaseMorphPivot {
