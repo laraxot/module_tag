adaa3aa6ca4ab156da7290bcaa6f8dcef018679f
Malebestia <malebestia@yahoo.it>
2022-06-14 13:59:30 +0200

    .

--- Resources/lang/it/tag.php
+++ Resources/lang/it/tag.php
@@ -3,6 +3,13 @@
 declare(strict_types=1);
 
 return [
+    'tags' => [
+        'tab' => [
+            'index' => 'Lista',
+            'edit' => 'Modifica',
+            'create' => 'Aggiungi',
+        ],
+    ],
     'tab' => [
         'index' => 'Lista',
         'edit' => 'Modifica',
