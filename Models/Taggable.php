a91a8c9c07177220b42024ef85a356aea7ce6044
marco xot <marco.sottana@gmail.com>
2022-06-15 19:12:58 +0200

    up

--- Models/Taggable.php
+++ Models/Taggable.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Modules\Tag\Models;
 
 /**
- * Modules\Tag\Models\Taggable.
+ * Modules\Tag\Models\Taggable
  *
- * @property int    $tag_id
+ * @property int $tag_id
  * @property string $taggable_type
- * @property int    $taggable_id
- *
+ * @property int $taggable_id
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Taggable query()
