4f6b2395bb3eff8610fe6ac6f39aa96828d315b3
Malebestia <malebestia@yahoo.it>
2022-06-19 19:27:26 +0200

    .

--- Models/Panels/TagMorphPanel.php
+++ Models/Panels/TagMorphPanel.php
@@ -1,25 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Tag\Models\Panels;
 
 use Illuminate\Http\Request;
-use Modules\Xot\Contracts\RowsContract;
-//--- Services --
+use Modules\Tag\Models\TagMorph;
+// --- Services --
 
+use Modules\Xot\Contracts\RowsContract;
 use Modules\Xot\Models\Panels\XotBasePanel;
 
 class TagMorphPanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
-     *
-     * @var string
      */
     public static string $model = 'TagMorph';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
      */
     public static string $title = 'title';
 
@@ -28,24 +27,22 @@ class TagMorphPanel extends XotBasePanel {
      *
      * @var array
      */
-    public static $search = array (
-);
+    public static $search = [
+    ];
 
     /**
      * The relationships that should be eager loaded on index queries.
-     *
      */
-    public function with():array {
+    public function with(): array {
         return [];
     }
 
-    public function search() :array {
-
+    public function search(): array {
         return [];
     }
 
-    /**
-     * on select the option id
+    /*
+     * on select the option id.
      *
      * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
@@ -53,17 +50,19 @@ class TagMorphPanel extends XotBasePanel {
      * @param TagMorph $row
      *
      * @return int|string|null
-     */
+
     public function optionId($row) {
         return $row->getKey();
     }
+    */
 
-    /**
+    /*
      * on select the option label.
-     */
+
     public function optionLabel($row):string {
         return (string)$row->title;
     }
+    */
 
     /**
      * index navigation.
@@ -79,96 +78,75 @@ class TagMorphPanel extends XotBasePanel {
      *
      * @return RowsContract
      */
-    public static function indexQuery(array $data, $query)
-    {
-        //return $query->where('user_id', $request->user()->id);
+    public static function indexQuery(array $data, $query) {
+        // return $query->where('user_id', $request->user()->id);
         return $query;
     }
 
-
-
     /**
      * Get the fields displayed by the resource.
-     *
-     * @return array
-        'col_size' => 6,
-        'sortable' => 1,
-        'rules' => 'required',
-        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
         'value'=>'..',
      */
     public function fields(): array {
-        return array (
-  0 => 
-  (object) array(
-     'type' => 'Id',
-     'name' => 'id',
-     'comment' => NULL,
-  ),
-  1 => 
-  (object) array(
-     'type' => 'Integer',
-     'name' => 'tag_id',
-     'rules' => 'required',
-     'comment' => NULL,
-  ),
-  2 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'post_type',
-     'comment' => NULL,
-  ),
-  3 => 
-  (object) array(
-     'type' => 'Bigint',
-     'name' => 'post_id',
-     'comment' => NULL,
-  ),
-  4 => 
-  (object) array(
-     'type' => 'Datetime',
-     'name' => 'created_at',
-     'comment' => NULL,
-  ),
-  5 => 
-  (object) array(
-     'type' => 'Datetime',
-     'name' => 'updated_at',
-     'comment' => NULL,
-  ),
-  6 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'created_by',
-     'comment' => NULL,
-  ),
-  7 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'updated_by',
-     'comment' => NULL,
-  ),
-  8 => 
-  (object) array(
-     'type' => 'Integer',
-     'name' => 'user_id',
-     'comment' => NULL,
-  ),
-  9 => 
-  (object) array(
-     'type' => 'Text',
-     'name' => 'note',
-     'comment' => NULL,
-  ),
-);
+        return [
+            0 => (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
+            1 => (object) [
+                'type' => 'Integer',
+                'name' => 'tag_id',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+            2 => (object) [
+                'type' => 'String',
+                'name' => 'post_type',
+                'comment' => null,
+            ],
+            3 => (object) [
+                'type' => 'Bigint',
+                'name' => 'post_id',
+                'comment' => null,
+            ],
+            4 => (object) [
+                'type' => 'Datetime',
+                'name' => 'created_at',
+                'comment' => null,
+            ],
+            5 => (object) [
+                'type' => 'Datetime',
+                'name' => 'updated_at',
+                'comment' => null,
+            ],
+            6 => (object) [
+                'type' => 'String',
+                'name' => 'created_by',
+                'comment' => null,
+            ],
+            7 => (object) [
+                'type' => 'String',
+                'name' => 'updated_by',
+                'comment' => null,
+            ],
+            8 => (object) [
+                'type' => 'Integer',
+                'name' => 'user_id',
+                'comment' => null,
+            ],
+            9 => (object) [
+                'type' => 'Text',
+                'name' => 'note',
+                'comment' => null,
+            ],
+        ];
     }
 
     /**
      * Get the tabs available.
-     *
-     * @return array
      */
-    public function tabs():array {
+    public function tabs(): array {
         $tabs_name = [];
 
         return $tabs_name;
@@ -176,10 +154,8 @@ class TagMorphPanel extends XotBasePanel {
 
     /**
      * Get the cards available for the request.
-     *
-     * @return array
      */
-    public function cards(Request $request):array {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -187,28 +163,22 @@ class TagMorphPanel extends XotBasePanel {
      * Get the filters available for the resource.
      *
      * @param \Illuminate\Http\Request $request
-     *
-     * @return array
      */
-    public function filters(Request $request = null):array {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
-     *
-     * @return array
      */
-    public function lenses(Request $request):array {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
-     *
-     * @return array
      */
-    public function actions():array {
+    public function actions(): array {
         return [];
     }
 }
