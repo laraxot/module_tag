b94406fbc7398d1f2747c8b9ee9971d688d3ade5
marco xot <marco.sottana@gmail.com>
2022-08-12 16:14:44 +0000

    up

--- Models/Tag.php
+++ Models/Tag.php
@@ -16,20 +16,14 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
  * Modules\Tag\Models\Tag
  *
  * @property int $id
- * @property string $tag_type
- * @property int $tag_cat_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property int $old_id
- * @property int $pos
- * @property int $tag_cat_id_up
- * @property string|null $created_by
- * @property string|null $updated_by
- * @property int $parent_id
  * @property array $name
  * @property array $slug
  * @property string|null $type
  * @property int|null $order_column
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
  * @property string|null $color
  * @method static Builder|Tag containing(string $name, $locale = null)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
@@ -41,14 +35,8 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereOldId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereOrderColumn($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereParentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag wherePos($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereTagCatId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereTagCatIdUp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereTagType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedBy($value)
